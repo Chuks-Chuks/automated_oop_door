89f4edd4b7f130e7dcfcf1ad845fb12b2a053be2
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2022-09-04 22:22:38 +0100

    Initial commit

---
+++ door_parts.py
@@ -0,0 +1,40 @@
+import time
+
+
+class Door:
+    def __init__(self):
+        self.access_code = self.set_up_code()
+        self.confirm_code()
+        self.count = 0
+
+    def set_up_code(self):
+        """This function returns the code set by the user"""
+        access_code = int(input("SET UP CODE: "))
+        if access_code:
+            return access_code
+        else:
+            print("NO INPUT RECEIVED")
+            self.set_up_code()
+
+    def confirm_code(self):
+        """This function returns the confirmation of the entered code."""
+        confirm_lock_code = int(input("RECONFIRM CODE:  "))
+        if confirm_lock_code == self.access_code:
+            print("MATCH")
+            return confirm_lock_code
+        else:
+            print("NO MATCH")
+            self.confirm_code()
+
+    def access_door(self):
+        """Returns the code the user enters to check against the registered code."""
+        code = int(input("ENTER CODE:  "))
+        if code == self.access_code:
+            print("✔")
+            return True
+        else:
+            print("❌")
+            self.count += 1
+            if self.count == 4:
+                time.sleep(10)
+            self.access_door()
---
+++ main.py
@@ -0,0 +1,5 @@
+# This is a simple project to build a door lock
+from door_parts import Door
+print("Welcome to Philip's Door Lock")
+safe = Door()
+safe.access_door()
